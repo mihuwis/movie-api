a681546313954a8dd7b7b7e4dcef68751b245677
Michal Wisniewski <mihuwis@gmail.com>
2019-10-21 19:00:56 +0200

    First commit

---
+++ src/test/java/com/progresspoint/movieapi/MovieApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progresspoint.movieapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
